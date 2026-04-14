157cce8eff40faf454b56b3ebb9eb4874f94f8da
Mikeq13 <13jmmq@gmail.com>
2026-04-14 17:01:53 -0600

    Disable keyboard dismiss wrapper on web

--- src/components/KeyboardDismissView.tsx
+++ src/components/KeyboardDismissView.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+import { Keyboard, Platform, TouchableWithoutFeedback, View } from 'react-native';
 
 export const KeyboardDismissView = ({ children }: { children: React.ReactNode }) => {
+  if (Platform.OS === 'web') {
+    return <View style={{ flex: 1 }}>{children}</View>;
+  }
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <View style={{ flex: 1 }}>{children}</View>
