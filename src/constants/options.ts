5f6aaa98771af3d32290f0c010e0cb077d4b8618
Mikeq13 <13jmmq@gmail.com>
2026-04-14 23:23:11 -0600

    Refine insights filters for demo reporting

--- src/constants/options.ts
+++ src/constants/options.ts
@@ -1,7 +1,7 @@
 import { BodyType, CollarType, FlyIntent, TailType } from '@/types/fly';
 import { Confidence, DepthRange, InsectStage, InsectType, WaterType } from '@/types/session';
 
-export const WATER_TYPES: WaterType[] = ['glide', 'lake', 'pocket water', 'pool', 'riffle', 'run'];
+export const WATER_TYPES: WaterType[] = ['glide', 'pocket water', 'pool', 'riffle', 'run'];
 export const DEPTH_RANGES: DepthRange[] = ['<1.5 ft', '1.5-3 ft', '3-5 ft', '>5 ft'];
 export const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] as const;
 export const INSECT_TYPES: InsectType[] = ['ant', 'beetle', 'caddis', 'grasshopper', 'leech', 'mayfly', 'midge', 'scud/sowbug', 'stonefly', 'yellow sally'];
